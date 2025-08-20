6380050d18efaed10316139ecab03c4160800f2c
SofKapa <sofiakmachado@gmail.com>
2025-08-20 22:34:09 +1000

    Testimonial Section

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 // Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
 
+
+  // AUDIENCE LIST
+
   // Select all audience items
   const items = document.querySelectorAll('.audience-item');
 
@@ -18,5 +21,28 @@ document.addEventListener('DOMContentLoaded', function() {
   items.forEach(item => observer.observe(item));
 
   
+
+  // TESTIMONIALS
+
+  const prevBtn = document.querySelector(".scroll-btn.prev");
+  const nextBtn = document.querySelector(".scroll-btn.next");
+  const wrapper = document.querySelector(".testimonial-wrapper");
+  const cards = wrapper.querySelectorAll(".testimonial-card");
+
+  let index = 0;
+
+  function showCard(i) {
+    index = i;
+    if (index < 0) index = 0;
+    if (index >= cards.length) index = cards.length -1;
+    wrapper.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  nextBtn.addEventListener('click', () => showCard(index + 1));
+  prevBtn.addEventListener('click', () => showCard(index - 1));
+
+  // optional: initialize first card
+  showCard(0);
+
 
 });
